74ecd4fe41fc0958787af5de172b16435e65b081
Crystalised <ollieform@gmail.com>
2022-06-10 20:22:15 +0100

    Fixed Discord, improved steam install.

--- heroic.sh
+++ heroic.sh
@@ -1,2 +1,2 @@
-wget https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.3.7/heroic_2.3.7_amd64.deb
+wget "https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.3.7/heroic_2.3.7_amd64.deb"
 sudo apt install ./heroic_2.3.7_amd64.deb
--- steam.sh
+++ steam.sh
@@ -1,4 +1,4 @@
-wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb
-sudo apt install ./steam.deb
 sudo apt-get install -y software-properties-common
-sudo apt install libc6:amd64 libc6:i386 libegl1:amd64 libegl1:i386 libgbm1:amd64 libgbm1:i386 libgl1-mesa-dri:amd64 libgl1-mesa-dri:i386 libgl1:amd64 libgl1:i386 steam-libs-amd64:amd64 steam-libs-i386:i386
+sudo add-apt-repository -y universe
+sudo add-apt-repository -y multiverse
+sudo apt-get install -y steam
